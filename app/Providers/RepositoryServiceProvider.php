d6424de2eb2d8b9fd0de09a2b799f68b19f88de5
Diether <llenaresdiether@gmail.com>
2020-06-15 01:43:38 -0700

    Initiated repository pattern

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Repositories\AppRepository;
+use App\Repositories\Interfaces\AppRepositoryInterface;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(
+            AppRepositoryInterface::class, 
+            AppRepository::class
+        );
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
---
+++ app/Repositories/AppRepository.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Repositories\Interfaces\AppRepositoryInterface;
+use App\User;
+
+class AppRepository implements AppRepositoryInterface {
+    public function getUserByID(number $id) {
+        
+    }
+}
